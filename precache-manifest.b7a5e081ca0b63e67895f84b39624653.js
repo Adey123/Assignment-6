a9287811558782dc4e2f4866574624a28f8d7de8
Adey123 <ednaharriskankam@gmail.com>
2020-09-10 18:04:18 +0000

    Updates

---
+++ precache-manifest.b7a5e081ca0b63e67895f84b39624653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d8d8eefed541247d7c117f6845e666",
+    "url": "/Assignment-6/index.html"
+  },
+  {
+    "revision": "9bac7e4e49699c07f77e",
+    "url": "/Assignment-6/static/css/main.5a5ff600.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/Assignment-6/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment-6/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bac7e4e49699c07f77e",
+    "url": "/Assignment-6/static/js/main.fb0abd6f.chunk.js"
+  },
+  {
+    "revision": "6fafc83034efe552ac5b",
+    "url": "/Assignment-6/static/js/runtime-main.da0db513.js"
+  },
+  {
+    "revision": "2b5a3aedf75f61d528297636b55e1c79",
+    "url": "/Assignment-6/static/media/App.2b5a3aed.PNG"
+  }
+]);
